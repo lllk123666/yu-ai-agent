aad0de8b2431ef6e5da0c5101531b7f82fb8bb08
lllk12345 <838002417@qq.com>
2026-04-29 23:55:10 +0800

    初始化图片搜索mcp

---
+++ yu-image-search-mcp/src/main/java/com/yupi/yuimagesearchmcp/YuImageSearchMcpApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.yuimagesearchmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuImageSearchMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuImageSearchMcpApplication.class, args);
+    }
+
+}
---
+++ yu-image-search-mcp/src/test/java/com/yupi/yuimagesearchmcp/YuImageSearchMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.yuimagesearchmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuImageSearchMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
